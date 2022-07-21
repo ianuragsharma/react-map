cd87d014d297315021f5ca8ef1f1f6936d417ff5
Anurag Sharma <anuragsharma0711@gmail.com>
2022-07-22 03:58:03 +0530

    feat: added weather report

---
+++ src/components/WeatherReport.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { weatherData } from "../data";
+const WeatherReport = () => {
+  const weatherList = weatherData.map((weather) => (
+    <div key={weather.id} className="weather-items">
+      <span className="weather-icon">{<weather.icon size={25} />}</span>
+      <span className="weather-icon">{weather.title}</span>
+      <div className="divider"></div>
+    </div>
+  ));
+  return <div className="weather-container">{weatherList}</div>;
+};
+
+export { WeatherReport };
